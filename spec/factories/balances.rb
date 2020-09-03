767a8787be0c1d8cd7582a1ff0a099489c04417c
David Elì <davidelimg1@gmail.com>
2020-09-02 23:47:59 -0500

    Fixed lint issues

--- spec/factories/balances.rb
+++ spec/factories/balances.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :balance do
-    user_id { Faker::Number.number(12) }
+    user_id { Faker::Number.number(digits: 12) }
     title { Faker::Lorem.word }
-    total { Faker::Number.number(8) }
+    total { Faker::Number.number(digits: 8) }
     category { Faker::Hipster.word }
   end
 end

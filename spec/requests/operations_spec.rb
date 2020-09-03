d81c4bfdb7d75060d111d6efba57df0e2b92efb1
David Elì <davidelimg1@gmail.com>
2020-09-03 16:57:02 -0500

    Refactored tests accodring to the new specifications

--- spec/requests/operations_spec.rb
+++ spec/requests/operations_spec.rb
@@ -65,51 +65,47 @@ RSpec.describe 'Operations API requested by USER' do
 
   # Test suite for PUT /balances/:balance_id/operations
   describe 'POST /balances/:balance_id/operations' do
+    let(:valid_attributes) { { title: 'Visit Narnia', status: 2 }.to_json }
 
     context 'when request attributes are valid' do
       before do
-        post "/users/#{user.id}/balances/#{balance_id}/operations", params: {}, headers: headers
+        post "/users/#{user.id}/balances/#{balance_id}/operations", params: valid_attributes, headers: headers
       end
 
-      it 'returns status code 401' do
-        expect(response).to have_http_status(401)
-      end
-
-      it 'returns an authorization failure message' do
-        expect(response.body)
-          .to match(/Unauthorized request/)
+      it 'returns status code 201' do
+        expect(response).to have_http_status(201)
       end
     end
 
     context 'when an invalid request' do
       before { post "/users/#{user.id}/balances/#{balance_id}/operations", params: {}, headers: headers }
 
-      it 'returns status code 401' do
-        expect(response).to have_http_status(401)
+      it 'returns status code 422' do
+        expect(response).to have_http_status(422)
       end
 
-      it 'returns an authorization failure message' do
-        expect(response.body)
-          .to match(/Unauthorized request/)
+      it 'returns a failure message' do
+        expect(response.body).to match(/Validation failed: Title can't be blank, Status can't be blank/)
       end
     end
   end
 
   # Test suite for PUT /balances/:balance_id/operations/:id
   describe 'PUT /balances/:balance_id/operations/:id' do
+    let(:valid_attributes) { { title: 'Mozart' }.to_json }
 
     before do
-      put "/users/#{user.id}/balances/#{balance_id}/operations/#{id}", params: {}, headers: headers
+      put "/users/#{user.id}/balances/#{balance_id}/operations/#{id}", params: valid_attributes, headers: headers
     end
 
     context 'when operation exists' do
-      it 'returns status code 401' do
-        expect(response).to have_http_status(401)
+      it 'returns status code 204' do
+        expect(response).to have_http_status(204)
       end
 
-      it 'returns an authorization failure message' do
-        expect(response.body)
-          .to match(/Unauthorized request/)
+      it 'updates the operation' do
+        updated_item = Operation.find(id)
+        expect(updated_item.title).to match(/Mozart/)
       end
     end
 
@@ -130,33 +126,25 @@ RSpec.describe 'Operations API requested by USER' do
   describe 'DELETE /balances/:id' do
     before { delete "/users/#{user.id}/balances/#{balance_id}/operations/#{id}", params: {}, headers: headers }
 
-    it 'returns status code 401' do
-      expect(response).to have_http_status(401)
-    end
-
-    it 'returns an authorization failure message' do
-      expect(response.body)
-        .to match(/Unauthorized request/)
+    it 'returns status code 204' do
+      expect(response).to have_http_status(204)
     end
   end
 end
 
 RSpec.describe 'Operations API requested by ADMIN' do
-  let(:user) { create(:user, id: 1, admin: true) }
+  let(:user) { create(:user, admin: true) }
   # Initialize the test data
   let!(:balance) { create(:balance, user_id: user.id) }
   let!(:operations) { create_list(:operation, 20, balance_id: balance.id, status: 1) }
   let(:balance_id) { balance.id }
   let(:id) { operations.first.id }
   let(:headers) { valid_headers }
-  let(:subject) { OperationsController.new }
 
   # Test suite for GET /balances/:balance_id/operations
   describe 'GET /balances/:balance_id/operations' do
     before { get "/users/#{user.id}/balances/#{balance_id}/operations", params: {}, headers: headers }
 
-    it { subject.class.skip_before_action :current_user? }
-    
     context 'when balance exists' do
       it 'returns status code 200' do
         expect(response).to have_http_status(200)
@@ -209,7 +197,7 @@ RSpec.describe 'Operations API requested by ADMIN' do
 
   # Test suite for PUT /balances/:balance_id/operations
   describe 'POST /balances/:balance_id/operations' do
-    let(:valid_attributes) { {title: 'Visit Narnia', status: 2 }.to_json }
+    let(:valid_attributes) { { title: 'Visit Narnia', status: 2 }.to_json }
 
     context 'when request attributes are valid' do
       before do

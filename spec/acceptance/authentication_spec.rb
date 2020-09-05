b84da37ea2d8bf6f18102898cf61c750cc72faf8
David Elì <davidelimg1@gmail.com>
2020-09-05 02:25:39 -0500

    Working on docs

--- spec/acceptance/authentication_spec.rb
+++ spec/acceptance/authentication_spec.rb
@@ -1,36 +1,38 @@
 require 'acceptance_helper'
 
-resource 'Authentication', acceptance: true do
-  let!(:user) { create(:user) }
-  before do
-    header 'Content-Type', 'application/json'
-  end
+resource 'Authentication' do
+  let(:user) { create(:user) }
+  explanation 'Authentication resources. '
+
+  header 'Content-Type', 'application/json'
 
   post 'signup' do
-    route_summary 'This is used to create users.'
 
     parameter :name, 'User name'
     parameter :email, 'User email'
     parameter :password, 'User password'
     parameter :password_confirmation, 'User password confirmation'
 
-    example_request 'Creating a new  User' do
-      explanation 'Registers a new user in the database'
-      do_request(name: 'David', email: 'david@gmail.com', password: 'foobar', password_confirmation: 'foobar')
-      expect(status).to eq(201)
+    context '201' do
+      example_request 'Creating new user - Sign Up' do
+        explanation 'Registers a new user in the database and generates token'
+        do_request(name: 'Example', email: 'example-x@example.com', password: 'foobar', password_confirmation: 'foobar')
+        expect(status).to eq(201)
+      end
     end
   end
 
   post 'auth/login' do
-    route_summary 'This is used to login users.'
 
     parameter :email, 'User email'
     parameter :password, 'User password'
 
-    example_request 'Creating a new token' do
-      explanation 'Logs in a registered user.'
-      do_request(email: user.email, password: user.password)
-      expect(status).to eq(200)
+    context '200' do
+      example_request 'Generating new token - Log In' do
+        explanation 'Logs in as a registered user and generates token.'
+        do_request(email: user.email, password: user.password)
+        expect(status).to eq(200)
+      end
     end
   end
 end
--- spec/acceptance/operations_spec.rb
+++ spec/acceptance/operations_spec.rb
@@ -1,32 +1,136 @@
 require 'acceptance_helper'
 
-resource 'Operation', acceptance: true do
-  let(:user) { create(:user, admin: true) }
-  let!(:balance) { create(:balance, user_id: user.id) }
-  let!(:operations) { create_list(:operation, 20, balance_id: 1, status: 1, user_id: user.id) }
-  let(:balance_id) { balance.id }
-  let(:id) { operations.first.id }
+resource 'Operations' do
+  let(:user) { create(:user) }
+  let(:balance) { create(:balance, user_id: user.id) }
+  let(:operation) {create(:operation, user_id: user.id, balance_id: balance.id ) }
+  let(:user_id) { user.id }
+  let(:operation_id) { operation.id }
   let(:headers) { valid_headers }
+  
+  explanation 'Operations resources. '
+
+  header "Content-Type", "application/json"
+
 
   get 'users/:user_id/operations' do
-    let(:user_id) { user.id }
-    example_request 'Listing operations items' do
-      explanation 'List all the operations from the given user'
-      expect(status).to eq 200
+
+    parameter :user_id, 'This parameter specifies the user from which we want to extract the operations.', required: true
+
+    context '200' do
+      example_request 'Get user operations.' do
+        explanation 'Get all the operations from a specified user. This an open endpoint to extract data.'
+        expect(status).to eq 200
+      end
     end
 
-    example_request 'returns all user operations' do
-      expect(JSON.parse(response_body).size).to eq(20)
+    context "404" do
+      let(:user_id) { 0 }
+      
+      example_request 'User is not found' do
+        expect(status).to eq(404)
+      end
     end
   end
+
+  get 'users/:user_id/operations/:operation_id' do
+
+    parameter :user_id, 'This parameter specifies the user from which we want to extract the operations.', required: true
+    parameter :operation_id, 'Specifies which specific operation we want to extract.', required: true
+
+    context '200' do
+      example_request 'Get specific user operation.' do
+        explanation 'Get a specific operation from the specified user. This an open endpoint to extract data.'
+        expect(status).to eq 200
+      end
+    end
+
+    context "404" do
+      let(:operation_id) { 0 }
+      
+      example_request 'Operation is not found' do
+        expect(status).to eq(404)
+      end
+    end
+  end
+
+  # post 'users/:user_id/operations' do
+
+  #   parameter :user_id, 'This parameter specifies the user from which we want to extract the operations.', required: true
+  #   parameter :title, 'The title of the operation', required: true
+  # #   parameter :status, 'Active or passive', required: true
+  # #   parameter :balance_id, 'Balance the operation belongs to', required: true
+
+  #   context '200' do
+  #     example_request 'Get user operations.' do
+  #       explanation 'Get all the operations from a specified user. This an open endpoint to extract data.'
+  #       expect(status).to eq 200
+  #     end
+  #   end
+
+  #   context "404" do
+  #     let(:user_id) { 0 }
+      
+  #     example_request 'User is not found' do
+  #       expect(status).to eq(404)
+  #     end
+  #   end
+  # end
+
+  # post 'users/:user_id/operations' do
+
+  #   parameter :title, 'The title of the operation', required: true
+  #   parameter :status, 'Active or passive', required: true
+  #   parameter :balance_id, 'Balance the operation belongs to', required: true
+
+  #   context '201' do
+  #     example_request 'Creating a new item' do
+  #       explanation 'Adds a new item to the selected todo'
+  #       do_request(title: 'Repetitions', status: false, balance_id: balance.id)
+  #       expect(status).to eq(201)
+  #     end
+  #   end
+  # end
+
+  # post 'users/:user_id/operations' do
+
+  #   parameter :title, 'The title of the operation', required: true
+  #   parameter :status, 'Active or passive', required: true
+  #   parameter :balance_id, 'Balance the operation belongs to', required: true
+
+  #   context "201" do
+  #     example_request 'Create an operation' do
+  #       explanation 'Get a specific operation from the specified user. This an open endpoint to extract data.'
+  #       expect(status).to eq(201)
+  #     end
+  #   end
+
+    # context "400" do
+    #   let(:id) { "a" }
+
+    #   example_request 'Invalid request' do
+    #     expect(status).to eq(400)
+    #   end
+    # end
+    
+    # context "404" do
+    #   let(:id) { 0 }
+      
+    #   example_request 'Order is not found' do
+    #     expect(status).to eq(404)
+    #   end
+    # end
+  # end
 
   # get 'projects/:project_id/todos/:todo_id/items/:id' do
   #   let(:project_id) { project.id }
   #   let(:todo_id) { todo.id }
   #   let(:id) { todos.first.id }
-  #   example_request 'Getting a specific item from the the todo list' do
-  #     explanation 'Shows a specific item from the todo list'
-  #     expect(status).to eq(200)
+  #   context '200' do
+  #     example_request 'Getting a specific item from the the todo list' do
+  #       explanation 'Shows a specific item from the todo list'
+  #       expect(status).to eq(200)
+  #     end
   #   end
   # end
 

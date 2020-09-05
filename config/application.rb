6c3e17b71bb288c08d23134e34bc621abae8353b
David Elì <davidelimg1@gmail.com>
2020-09-04 23:03:57 -0500

    Adding auth folder to app config

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,6 @@ module BalancectrlApi
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
     config.autoload_paths << Rails.root.join('lib')
+    config.autoload_paths << Rails.root.join('auth')
   end
 end

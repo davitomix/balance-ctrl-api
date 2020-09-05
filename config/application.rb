b5400aa15047920129dc43dd0fc3b4b7e59a5309
David Elì <davidelimg1@gmail.com>
2020-09-04 22:59:18 -0500

    Changin configuration to load lib folder

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,6 @@ module BalancectrlApi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
@@ -31,5 +30,6 @@ module BalancectrlApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.autoload_paths << Rails.root.join('lib')
   end
 end

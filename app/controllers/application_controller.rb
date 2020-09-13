ca0ac47259c3387dd72894a27ec787a50e5a5ff1
David Elì <davidelimg1@gmail.com>
2020-09-12 20:32:41 -0500

    Fixed lint issues

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,9 +14,11 @@ class ApplicationController < ActionController::API
     @current_user = AuthorizeApiRequest.new(request.headers).call[:user]
   end
 
+  # rubocop:disable Style/GuardClause
   def authorize_admin_request
     unless AuthorizeApiRequest.new(request.headers).call[:admin]
       raise(ExceptionHandler::AuthenticationError, Message.unauthorized)
     end
   end
+  # rubocop:enable Style/GuardClause
 end

2f08a544bf048779bc4a9c3e5d29190e2cbdce1f
David Martinez <davidelimg1@gmail.com>
2021-05-29 00:56:14 -0500

    [chore]: Cleaned up users & signup controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,13 +6,13 @@ class UsersController < ApplicationController
     user = User.create!(user_params)
     auth_token = AuthenticateUser.new(user.email, user.password).call
     response = { message: Message.account_created, auth_token: auth_token, user_id: user.id }
-    json_response(response, :created)
+    render json: response, status: :created
   end
 
   private
 
   def user_params
-    params.permit(
+    params.require(:user).permit(
       :name,
       :email,
       :password,
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,42 +1,52 @@
-# rubocop:disable Layout/LineLength
 require 'rails_helper'
 
 RSpec.describe 'Users API', type: :request do
   let!(:user) { User.new(password: 'password', password_confirmation: 'password') }
   let(:headers) { valid_headers.except('Authorization') }
-  let(:valid_attributes) do
-    { name: 'Mr Smith', email: 'mrsemith@example.com', password: 'password', password_confirmation: 'password' }
-  end
 
-  # User signup test suite
   describe 'POST /signup' do
     context 'when valid request' do
-      before { post '/signup', params: valid_attributes.to_json, headers: headers }
-
       it 'creates a new user' do
+        post '/signup',
+             headers: headers,
+             params: {
+               user: {
+                 name: 'Mr Smith',
+                 email: 'mrsemith@example.com',
+                 password: 'password',
+                 password_confirmation: 'password'
+               }
+             }.to_json
+
+        data = JSON.parse(response.body)
+
         expect(response).to have_http_status(201)
-      end
-
-      it 'returns success message' do
-        expect(json['message']).to match(/Account created successfully/)
-      end
-
-      it 'returns an authentication token' do
-        expect(json['auth_token']).not_to be_nil
+        expect(data['message']).to match(/Account created successfully/)
+        expect(data['auth_token']).not_to be_nil
+        expect(data['user_id']).to eq(34)
       end
     end
 
     context 'when invalid request' do
-      before { post '/signup', params: {}, headers: headers }
-
-      it 'does not create a new user' do
-        expect(response).to have_http_status(422)
-      end
       it 'returns failure message' do
-        expect(json['message'])
-          .to match(/Password can't be blank, Name can't be blank, Name is invalid, Email can't be blank, Email is invalid/)
+        post '/signup',
+             headers: headers,
+             params: {
+               user: {
+                 name: nil,
+                 email: nil,
+                 password: nil,
+                 password_confirmation: nil
+               }
+             }.to_json
+
+        data = JSON.parse(response.body)
+
+        expect(response).to have_http_status(422)
+        expect(data['message']).not_to be_nil
+        expect(data['auth_token']).to be_nil
+        expect(data['user_id']).to be_nil
       end
     end
   end
 end
-# rubocop:enable Layout/LineLength

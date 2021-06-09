385e369390dc083850ce7d517ac3a96dc08ce976
David Martinez <davidelimg1@gmail.com>
2021-06-08 23:20:24 -0500

    [chore]: Implemented tests for balances controller

--- spec/requests/api/balances_controller_spec.rb
+++ spec/requests/api/balances_controller_spec.rb
@@ -37,25 +37,25 @@ RSpec.describe BalancesController, type: :controller do
       before { log_in_as(user) }
 
       it 'returns a success response' do
-        balance_1 = BalanceFactory.create(user_id: user.id)
+        balance = BalanceFactory.create(user_id: user.id)
 
         get :show, params: {
-          id: balance_1.id
+          id: balance.id
         }
 
         data = JSON.parse(response.body)
 
         expect(response).to have_http_status(:ok)
-        expect(data['balance']).to eq(JSON.parse(balance_1.to_json))
+        expect(data['balance']).to eq(JSON.parse(balance.to_json))
       end
     end
 
     context 'when not signed in' do
       it 'returns 401 code' do
-        balance_1 = BalanceFactory.create(user_id: user.id)
+        balance = BalanceFactory.create(user_id: user.id)
 
         get :show, params: {
-          id: balance_1.id
+          id: balance.id
         }
 
         expect(response).to have_http_status(:unauthorized)
@@ -104,6 +104,65 @@ RSpec.describe BalancesController, type: :controller do
             total: 12_350,
             category: 'Technological Surgery',
             user_id: user.id
+          }
+        }
+
+        expect(response).to have_http_status(:unauthorized)
+      end
+    end
+  end
+
+  describe 'PUT /balance/:id' do
+    let!(:user) { UserFactory.create }
+
+    context 'when signed in user' do
+      before { log_in_as(user) }
+
+      it 'returns a success response' do
+        balance = BalanceFactory.create
+
+        put :update, params: {
+          id: balance.id,
+          balance: {
+            title: 'Industrial Shipment',
+            total: 12_350,
+            category: 'Technological Surgery',
+            user_id: user.id
+          }
+        }
+
+        expect(response).to have_http_status(:no_content)
+      end
+    end
+
+    context 'when balance not exists' do
+      before { log_in_as(user) }
+
+      it 'returns 404' do
+        put :update, params: {
+          id: 100,
+          balance: {
+            title: 'Industrial Shipment',
+            total: 12_350,
+            category: 'Technological Surgery',
+            user_id: user.id
+          }
+        }
+
+        expect(response).to have_http_status(:not_found)
+      end
+    end
+
+    context 'when not signed in' do
+      it 'returns 401 code' do
+        balance = BalanceFactory.create
+
+        put :update, params: {
+          id: balance.id,
+          balance: {
+            title: 'Industrial Shipment',
+            total: 12_350,
+            category: 'Technological Surgery'
           }
         }
 

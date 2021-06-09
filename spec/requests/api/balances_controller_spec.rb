517675f65ff822fa272fd78b58aaf027ca53e155
David Martinez <davidelimg1@gmail.com>
2021-06-08 22:17:35 -0500

    [chore]: Fixed lint issues

--- spec/requests/api/balances_controller_spec.rb
+++ spec/requests/api/balances_controller_spec.rb
@@ -54,7 +54,7 @@ RSpec.describe BalancesController, type: :controller do
       it 'returns 401 code' do
         balance_1 = BalanceFactory.create(user_id: user.id)
 
-        get :show, params: { 
+        get :show, params: {
           id: balance_1.id
         }
 
@@ -66,7 +66,7 @@ RSpec.describe BalancesController, type: :controller do
       before { log_in_as(user) }
 
       it 'returns 404' do
-        get :show, params: { 
+        get :show, params: {
           id: 100
         }
 
@@ -82,16 +82,14 @@ RSpec.describe BalancesController, type: :controller do
       before { log_in_as(user) }
 
       it 'returns a success response' do
-        post :create, params: { 
-          balance: { 
+        post :create, params: {
+          balance: {
             title: 'Industrial Shipment',
             total: 12_350,
             category: 'Technological Surgery',
             user_id: user.id
           }
         }
-
-        data = JSON.parse(response.body)
 
         expect(response).to have_http_status(:created)
         expect(response.body).to include(user.id.to_json)
@@ -100,10 +98,8 @@ RSpec.describe BalancesController, type: :controller do
 
     context 'when not signed in' do
       it 'returns 401 code' do
-        balance_1 = BalanceFactory.create(user_id: user.id)
-
-        post :create, params: { 
-          balance: { 
+        post :create, params: {
+          balance: {
             title: 'Industrial Shipment',
             total: 12_350,
             category: 'Technological Surgery',
